958ff31491cf9d4e1926c953744676a85ee3b8c5
najibu <ns.najibu@gmail.com>
2017-03-12 14:26:14 +0000

    Added specification pattern

---
+++ specification_pattern/tests/CustomersRepositoryTest.php
@@ -0,0 +1,38 @@
+<?php 
+
+class CustomersRepositoryTest extends PHPUnit_Framework_TestCase
+{
+  protected $customers;
+
+  public function setUp()
+  {
+    $customers = new CustomersRepository(
+        [
+          new Customer('gold'),
+          new Customer('bronze'),
+          new Customer('silver'),
+          new Customer('gold')
+        ]
+    );
+  }
+    /*
+  @test
+   */
+  function it_fetches_all_customers()
+  {
+    $results = $this->customers->all();
+
+    $this->assertCount(4, $results); 
+  }
+
+  /*
+  @test
+   */
+  function it_fetches_all_customers_who_match_a_given_specification()
+  {
+   
+    $results = $customers->matchingSpecification(new CustomerIsGold);
+
+    $this->assertCount(2, $results);
+  }
+}
